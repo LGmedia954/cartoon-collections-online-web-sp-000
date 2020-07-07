7d8236e416b14f4805be83f387a90b56e4b053aa
Leslie G <lesliegonzalez@me.com>
2020-07-07 22:25:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,6 +34,7 @@ long_planeteer_calls(planeteer_calls)
 
 
 snacks = ["crackers", "gouda", "thyme"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 
 def find_the_cheese# code an argument here
   # the array below is here to help
