42c77eddc9f8080bdd36ee62dc77912971e74b68
Leslie G <lesliegonzalez@me.com>
2020-07-07 22:25:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,6 +33,8 @@ long_planeteer_calls(planeteer_calls)
 
 
 
+snacks = ["crackers", "gouda", "thyme"]
+
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
